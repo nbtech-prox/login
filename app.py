a122b027b5c701154560fb5169ae7ca8e8d8fb81
nbtech-prox <nbtech.prox@gmail.com>
2024-09-29 17:14:10 +0100

    continuação do back-end
    Foi criado pasta templates e dentro dela os ficheiros index e registrado em html, foi instalado as dependencias  flask_wtf e email-validataor.

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-from flask import Flask
+from os import name
+from flask import Flask, render_template, request, redirect
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, SubmitField
+from wtforms.validators import DataRequired, InputRequired, EqualTo, Email
 
+# Criando a aplicação Flask
 app = Flask(__name__)
+app.config['SECRET_KEY']='abc123abc'
 
-@app.route('/')
-def hello():
-    return "<H1>Olá Nuno Batista!</H1>"
+class RegisterForm(FlaskForm):
+    first_name = StringField('Primeiro nome', validators=[DataRequired()])
+    second_name = StringField('Apelido', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[Email(message='E-mail inválido!')])
+    password = PasswordField('Password', validators=[InputRequired(), EqualTo('confirm', message='A password devem ser iguais.')])
+    confirm = PasswordField('Corfime a Password')
+    submit = SubmitField('REGISTRAR')
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+       return redirect('/registrado')
+
+    return render_template('index.html', form=form)
+@app.route('/registrado')
+def registrado():
+    return render_template('registrado.html', name='Nuno Batista')
+
+# Executando o servidor
 if __name__ == '__main__':
     app.run(debug=True, port=5152)
