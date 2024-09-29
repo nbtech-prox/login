13b17ee3e4f87d81acd52c805dee9a2db7a28324
nbtech-prox <nbtech.prox@gmail.com>
2024-09-29 13:41:07 +0100

    Base de Flask

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "<H1>Olá Nuno Batista!</H1>"
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5152)
